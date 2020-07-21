2637b101a5a859b8658decd6371d796d4879e24c
ItsLaro <ivan.reor@gmail.com>
2020-07-21 17:35:25 -0400

    Modified setPairingCodesJob to give new pairing codes to all users

--- cloud/main.js
+++ cloud/main.js
@@ -43,16 +43,15 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
   response.success('success');
 });
 
-Parse.Cloud.job("myJob", (request) =>  {
+Parse.Cloud.job("setPairingCodesJob", (request) =>  {
   // params: passed in the job call
   // headers: from the request that triggered the job
   // log: the ParseServer logger passed in the request
   // message: a function to update the status message of the job object
   const { params, headers, log, message } = request;
 
-  (10000000).toString(36);
-  message("I just started");
-  return getRandomPairingCode();
+  const query = new Parse.Query('User');
+  const result = await query.find();
 
   function getRandomPairingCode() {
     let max = 60466175;
@@ -60,6 +59,12 @@ Parse.Cloud.job("myJob", (request) =>  {
     let randNumberInRange = Math.random() * (max - min) + min
     return Math.floor(randNumberInRange).toString(36);
   }
+
+  for(let i = 0; i < result.length; i++){
+    result[i].set('myCode', getRandomPairingCode());
+    await result[i].save();
+  }
+
 });
 
 // iOS push testing
